f4936ec63dc2e4dca26dc455884d4b970fa269d5
DavidHenrique4 <156692594+DavidHenrique4@users.noreply.github.com>
2024-11-05 18:07:24 +0000

    as

---
+++ UF1/Practicas UF1/ejercicio 6 - MathStrings/index.js
@@ -0,0 +1,16 @@
+console.log("Hola")
+
+//1- Aleatorio
+//Parseint para sacar de texto a numero
+document.querySelector("#btnAleatorio").addEventListener("click", function(){
+    let num1 = parseInt(document.querySelector("#num1").value);
+    let num2 = parseInt(document.querySelector("#num2").value);
+    let numAleatorio = Math.floor(Math.random() * (num2 - num1 + 1))
+
+    document.querySelector("#numeroAleatorio").innerHTML =  numAleatorio;
+})
+
+//2-Strings 
+
+
+
