4e3f3f0ce204374bd22078246085cac5535896ec
DavidHenrique4 <156692594+DavidHenrique4@users.noreply.github.com>
2024-11-10 21:26:29 +0000

    4 y 5 update

--- UF1/Practicas UF1/ejercicio 6 - MathStrings/index.js
+++ UF1/Practicas UF1/ejercicio 6 - MathStrings/index.js
@@ -1,6 +1,6 @@
 console.log("Hola")
 
-//1- Aleatorio
+//1
 //Parseint para sacar de texto a numero
 document.querySelector("#btnAleatorio").addEventListener("click", function(){
     let num1 = parseInt(document.querySelector("#num1").value);
@@ -11,7 +11,39 @@ document.querySelector("#btnAleatorio").addEventListener("click", function(){
 })
 
 
-//2-Strings 
+//2
+document.querySelector("#btnMayus").addEventListener("click", function(){
+    const textArea = document.querySelector("#texto");
+    textArea.value = textArea.value.toUpperCase(); //topercase pa poner mayuscula
+  });
+
+  document.querySelector("#btnMinus").addEventListener("click", function(){
+    const textArea = document.querySelector("#texto");
+    textArea.value = textArea.value.toLowerCase(); //tolower pa minuscula
+  });
+
+
+
+  //4  
+  document.querySelector("#btn-contra").addEventListener("click", function(){
+    const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let contra = "";
+    for (let i = 0; i < 10; i++) {
+      contra += letras.charAt(Math.floor(Math.random() * letras.length));
+    }
+    document.querySelector("#pass").value = contra;
+  })
+
+
+  //5 
+  document.querySelector("#btn-emoji").addEventListener("click", function(){
+ const emoji =  ["😃", "😂", "😍", "😎", "😭", "😡", "😱", "😏"];
+ const emojiAle = Math.floor(Math.random() * emoji.length);
+ document.querySelector("#emoji").innerHTML=emoji[emojiAle];
+
+  })
+  
+
 
 
 
